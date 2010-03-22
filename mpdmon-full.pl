7c27d7abbda020a2918e6660a7f505773dd44902
trapd00r <trapd00r@trapd00r.se>
2010-03-22 07:54:41 +0100

    initial commit

---
+++ mpdmon-full.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl 
+use strict;
+# mpdmon-full
+# Copyright (C) trapd00r 2010
+# This version of mpdmon outputs [time] artist/album/song whenever song
+# changes, keeping a history of recently played tracks for reference.
+# If you want a realtime alternative, you'll want;
+# http://github.com/trapd00r/mpdmon-realtime
+
+use Audio::MPD;
+use List::Util qw(shuffle);
+my $mpd = Audio::MPD->new;
+
+sub monitor {
+  my $np = "";
+  my @c = ("\033[0m", "\033[31m", "\033[33m", "\033[34;1m");
+  while(1) {
+    my $current = $mpd->current;
+    my $output = $mpd->current->file;
+    if(!$current) {
+      $current = $c[1].'undef'.$c[0];
+      $output = $current;
+    }
+    my $time1 = $mpd->status->time->sofar;
+    my $time2 = $mpd->status->time->total;
+    my @date  = localtime(time);
+
+    if("$np" ne "$current") {
+      $np = $current;
+      my @rc = shuffle(@c);
+      printf("%02s:%02s:%02s▕ $rc[0]%s$c[0]\n",
+              $date[2], $date[0], $date[1], $output);
+    }
+    sleep 2;
+  }
+}
+
+&monitor;
+
